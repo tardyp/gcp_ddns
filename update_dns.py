737c00eb06a3dfb26ff5195d5154c678a19ab9da
Pierre Tardy <tardyp@gmail.com>
2020-02-16 18:25:29 +0100

    autoip

--- update_dns.py
+++ update_dns.py
@@ -2,11 +2,25 @@ import click
 from google.cloud import dns
 import copy
 import time
+import socket
+import fcntl
+import struct
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
 
 @click.command()
 @click.option('--ip', help='ip of the host.')
 @click.option('--name', help='nome of host')
 def update(ip, name):
+    if ip.startswith('eth'):
+        ip = get_ip_address(ip)
+
     client = dns.Client.from_service_account_json('auth.json')
     zones = client.list_zones()
     for zone in zones:
